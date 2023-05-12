1ee38860ca5b87b22845100c6b184772f0fd9541
João Victor Morais de Mello <32046489+jvictormello@users.noreply.github.com>
2023-05-12 05:38:46 -0300

    feat: Create reset endpoitn (#6)

---
+++ app/Http/Controllers/ResetController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Account\AccountServiceContract;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResetController extends Controller
+{
+    protected $accountService;
+
+    public function __construct(AccountServiceContract $accountServiceContract)
+    {
+        $this->accountService = $accountServiceContract;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function index()
+    {
+        try {
+            DB::table('accounts')->truncate();
+            DB::table('events')->truncate();
+
+            return response()->json('Success', Response::HTTP_OK);
+        } catch (Exception $exception) {
+            return response()->json(0, Response::HTTP_NOT_FOUND);
+        }
+    }
+}
