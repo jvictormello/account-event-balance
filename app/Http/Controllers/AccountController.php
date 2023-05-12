11de3b8087914038e4ed7c7c81ad1af313d50e48
jvictormello <jvictor.mello@gmail.com>
2023-05-12 07:12:00 -0300

    feat: Remove comments

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -27,20 +27,10 @@ class AccountController extends Controller
             $accountId = $request->has('account_id') ? $request->get('account_id') : null;
 
             return response()->json($this->accountService->getAccountBalanceByAccountId($accountId), Response::HTTP_OK);
-
-            // test this better response
-            // return (new AccountResource($this->accountService->getAccountByAccountId($accountId)))->response()->setStatusCode(Response::HTTP_OK);
         } catch (ModelNotFoundException $exception) {
             return response()->json(0, Response::HTTP_NOT_FOUND);
-
-            // test this better response
-            // return response()->json(['message' => $exception->getMessage()], Response::HTTP_NOT_FOUND);
         } catch (Exception $exception) {
             return response()->json(0, Response::HTTP_NOT_FOUND);
-
-            // test this better response
-            // $errorCode = $exception->getCode() ? $exception->getCode() : Response::HTTP_NOT_FOUND;
-            // return response()->json(['message' => $exception->getMessage()], $errorCode);
         }
     }
 }
