f61e31a8f0d98e6fa8736128bd563ff416fdc354
João Victor Morais de Mello <jvictor.mello@gmail.com>
2025-05-19 07:11:52 -0300

    feat: Treat errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,11 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Exception;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +30,48 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Invalid input (422)
+        if ($exception instanceof ValidationException) {
+            return response()->json(['error' => 'Invalid input.'], 422);
+        }
+
+        // Model not found (404)
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(0, 404);
+        }
+
+        // Non valid route (404)
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'error' => 'Endpoint not found.'
+            ], 404);
+        }
+
+        // No funds account (400) - Insufficient Balance
+        if ($exception instanceof Exception && str_contains($exception->getMessage(), 'Insufficient balance')) {
+            return response()->json(['error' => 'Insufficient balance.'], 400);
+        }
+
+        // Invalid transfer (400) - Same Account
+        if ($exception instanceof Exception && str_contains($exception->getMessage(), 'Invalid transfer')) {
+            return response()->json(['error' => 'Invalid transfer. Origin and destination cannot be the same.'], 400);
+        }
+
+        // Invalid Event (400)
+        if ($exception instanceof Exception && str_contains($exception->getMessage(), 'Invalid event type')) {
+            return response()->json(['error' => 'Invalid event type.'], 400);
+        }
+
+        // Default (500)
+        return response()->json([
+            'error' => 'Something went wrong. Please try again later.'
+        ], 500);
     }
 }
